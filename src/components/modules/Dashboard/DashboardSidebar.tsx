0f2fab6501ade1dcab44ff57c660bcefb24c52ab
ShafinRME <shafin.rmedu@gmail.com>
2026-03-09 19:33:00 +0600

    dashboard layout for different user is done

--- src/components/modules/Dashboard/DashboardSidebar.tsx
+++ src/components/modules/Dashboard/DashboardSidebar.tsx
@@ -1,9 +1,9 @@
 import { getDefaultDashboardRoute } from "@/lib/auth-utils";
-import { getNavItemsByRole } from "@/lib/navItems.config";
 import { getUserInfo } from "@/services/auth/getUserInfo";
 import { NavSection } from "@/types/dashboard.interface";
 import { UserInfo } from "@/types/user.interface";
 import DashboardSidebarContent from "./DashboardSidebarContent";
+import { getNavItemsByRole } from "@/lib/navItems.config";
 
 const DashboardSidebar = async () => {
     const userInfo = (await getUserInfo()) as UserInfo;
